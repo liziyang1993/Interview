244430021cef3493266c427627a1ea658f19bc8a
liziyang1993 <liziyang_1993@126.com>
2020-02-11 13:01:02 +0800

    dead lock

---
+++ src/com/lzy/lock/DeadLock.java
@@ -0,0 +1,53 @@
+package com.lzy.lock;
+
+import java.util.concurrent.TimeUnit;
+
+public class DeadLock {
+
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
+
+    public void testDeadLock(){
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread1 start ......");
+                synchronized (lock1){
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock2){
+                        System.out.println("Thread1 finished!");
+                    }
+
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread2 start ......");
+                synchronized (lock2){
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock1){
+                        System.out.println("Thread2 finished!");
+                    }
+
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
+
+    public static void main(String[] args){
+        new DeadLock().testDeadLock();
+    }
+}
